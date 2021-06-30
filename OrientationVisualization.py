9ae7a0cb02901cfd325a9070e8b8c25d201abd54
wenner <nelsondiasdemedeiros@gmail.com>
2021-06-30 12:00:00 -0300

    feat: add message_received

--- OrientationVisualization.py
+++ OrientationVisualization.py
@@ -15,3 +15,12 @@ class OrientationVisualization:
     server.send_message_to_all("Hey all, a new client has joined us")
   def client_left(self, client, server):
     print("[x] Client(%d) disconnected" % client['id'])
+
+  def message_received(self, client, server, message):
+    data_serialized = json.loads(message)
+    if self.useQuat:
+      [w, x, y, z] = data_serialized
+      self.data = [w, x, y, z]
+    else:
+      [pitch, roll, yaw] = data_serialized
+      self.data = [pitch, roll, yaw]
